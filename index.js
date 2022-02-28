3fa2433ede182b34d698329e68c9d0c19dda5ff3
Daninogamer <88000948+Daninogamer@users.noreply.github.com>
2022-02-28 11:51:07 +0100

    fix

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ client.on('message', message => {
 
     client.on('guildMemberAdd', member => {
         //Autoruolo
-    const ruolo = member.guild.roles.cache.find(r => r.name === 'Membro');
+    const ruolo = member.guild.roles.cache.find(r => r.name === 'Fungo Fungoso🍄');
     member.roles.add(ruolo);
 })
 
